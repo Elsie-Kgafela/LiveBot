83d197cb078cac4d1d319404f1c1b15a07a4869d
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:35:59 +0200

    update file

---
+++ LiveBot.Core/Contracts/Discord/IDiscordChannelUpdate.cs
@@ -0,0 +1,9 @@
+namespace LiveBot.Core.Contracts.Discord
+{
+    public interface IDiscordChannelUpdate
+    {
+        public ulong GuildId { get; set; }
+        public ulong ChannelId { get; set; }
+        public string ChannelName { get; set; }
+    }
+}
