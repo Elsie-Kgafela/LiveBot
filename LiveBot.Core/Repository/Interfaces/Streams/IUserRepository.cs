d4e1627e69cd71597819948a54b8f187bd142d63
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:20 +0200

    update file

---
+++ LiveBot.Core/Repository/Interfaces/Streams/IUserRepository.cs
@@ -0,0 +1,11 @@
+using LiveBot.Core.Repository.Models.Streams;
+
+namespace LiveBot.Core.Repository.Interfaces.Streams
+{
+    /// <summary>
+    /// Interface for Database interaction with a Stream User
+    /// </summary>
+    public interface IUserRepository : IRepository<StreamUser>
+    {
+    }
+}
