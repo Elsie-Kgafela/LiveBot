88be55ee31968f453f4a9ebb450bcfc583c59476
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:22 +0200

    update file

---
+++ LiveBot.Core/Repository/Models/Discord/DiscordGuild.cs
@@ -0,0 +1,20 @@
+using LiveBot.Core.Repository.Models.Streams;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace LiveBot.Core.Repository.Models.Discord
+{
+    public class DiscordGuild : BaseDiscordModel<DiscordGuild>
+    {
+        public string IconUrl { get; set; }
+        public virtual DiscordGuildConfig Config { get; set; }
+
+        public bool IsInBeta { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<DiscordChannel> DiscordChannels { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<StreamSubscription> StreamSubscriptions { get; set; }
+    }
+}
