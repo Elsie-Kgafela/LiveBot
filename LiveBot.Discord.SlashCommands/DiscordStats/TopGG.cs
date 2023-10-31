f3cea96ac61e282989c6de94d41b266d70020025
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:35:26 +0200

    update file

---
+++ LiveBot.Discord.SlashCommands/DiscordStats/TopGG.cs
@@ -0,0 +1,21 @@
+using Discord.WebSocket;
+
+namespace LiveBot.Discord.SlashCommands.DiscordStats
+{
+    /// <inheritdoc/>
+    public class TopGG : BaseStats
+    {
+        public TopGG(ILogger<TopGG> logger, IConfiguration configuration, DiscordShardedClient discordClient)
+            : base(logger, configuration, discordClient)
+        {
+            SiteName = "TopGG";
+            ApiConfigName = "TopGG_API";
+            UpdateUrl = "https://top.gg/api/bots/{BotId}/stats";
+            GuildCountFieldName = "server_count";
+        }
+
+        /// <inheritdoc/>
+        public override bool SetAuthorization(HttpClient httpClient, string apiKey) =>
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", apiKey);
+    }
+}
