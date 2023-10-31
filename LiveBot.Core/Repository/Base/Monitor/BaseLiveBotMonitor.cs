bf7ab94c7e94ab872e6194ca9aebb79281538d2e
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:14 +0200

    update file

---
+++ LiveBot.Core/Repository/Base/Monitor/BaseLiveBotMonitor.cs
@@ -0,0 +1,60 @@
+using LiveBot.Core.Repository.Interfaces;
+using LiveBot.Core.Repository.Interfaces.Monitor;
+using LiveBot.Core.Repository.Static;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LiveBot.Core.Repository.Base.Monitor
+{
+    /// <summary>
+    /// Represents a Base implementation of <c>ILiveBotMonitor</c>
+    /// </summary>
+    public abstract class BaseLiveBotMonitor : ILiveBotMonitor
+    {
+        protected BaseLiveBotMonitor()
+        {
+        }
+
+        public string URLPattern { get; set; }
+        public string BaseURL { get; set; }
+        public ServiceEnum ServiceType { get; set; }
+        public IUnitOfWorkFactory _factory { get; set; }
+
+        public IUnitOfWork _work
+        {
+            get => _factory.Create();
+        }
+
+        public DateTime StartTime { get; set; }
+        public bool IsEnabled { get; set; }
+
+        public Regex GetURLRegex(string pattern)
+        {
+            return new Regex(pattern, RegexOptions.IgnoreCase | RegexOptions.Compiled);
+        }
+
+        public abstract Task StartAsync(bool IsWatcher = false);
+
+        public abstract Task<ILiveBotGame> GetGame(string gameId);
+
+        public abstract Task<ILiveBotStream> GetStream(ILiveBotUser user);
+
+        public abstract Task<ILiveBotStream> GetStream(string userId);
+
+        public abstract Task<ILiveBotStream> GetStream_Force(ILiveBotUser user);
+
+        public abstract Task<ILiveBotUser> GetUserById(string userId);
+
+        public abstract Task<ILiveBotUser> GetUser(string username = null, string userId = null, string profileURL = null);
+
+        public bool IsValid(string streamURL)
+        {
+            return Regex.IsMatch(streamURL, URLPattern);
+        }
+
+        public abstract bool AddChannel(ILiveBotUser user);
+
+        public abstract bool RemoveChannel(ILiveBotUser user);
+    }
+}
