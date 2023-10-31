0c5aa15acff0c6018215789297cbb2eaea49dc88
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:35:26 +0200

    update file

---
+++ LiveBot.Discord.SlashCommands/DiscordStats/DiscordBots.cs
@@ -0,0 +1,21 @@
+using Discord.WebSocket;
+
+namespace LiveBot.Discord.SlashCommands.DiscordStats
+{
+    /// <inheritdoc/>
+    public class DiscordBots : BaseStats
+    {
+        public DiscordBots(ILogger<DiscordBots> logger, IConfiguration configuration, DiscordShardedClient discordClient)
+            : base(logger, configuration, discordClient)
+        {
+            SiteName = "DiscordBots";
+            ApiConfigName = "DiscordBots_API";
+            UpdateUrl = "https://discord.bots.gg/api/v1/bots/{BotId}/stats";
+            GuildCountFieldName = "guildCount";
+        }
+
+        /// <inheritdoc/>
+        public override bool SetAuthorization(HttpClient httpClient, string apiKey) =>
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Authorization", apiKey);
+    }
+}
