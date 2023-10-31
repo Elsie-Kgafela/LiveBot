f0998799b98dfb548b46cc3a0ec7b531dc06d079
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:03 +0200

    update file

---
+++ LiveBot.Core/Repository/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+using LiveBot.Core.Repository.Interfaces.Discord;
+using LiveBot.Core.Repository.Interfaces.Streams;
+
+namespace LiveBot.Core.Repository.Interfaces
+{
+    /// <summary>
+    /// Represents an interactable Database Instance
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        IGuildRepository GuildRepository { get; }
+        IChannelRepository ChannelRepository { get; }
+        ISubscriptionRepository SubscriptionRepository { get; }
+        IUserRepository UserRepository { get; }
+        INotificationRepository NotificationRepository { get; }
+        IGameRepository GameRepository { get; }
+        IAuthRepository AuthRepository { get; }
+        IGuildConfigRepository GuildConfigRepository { get; }
+        IRoleToMentionRepository RoleToMentionRepository { get; }
+    }
+}
