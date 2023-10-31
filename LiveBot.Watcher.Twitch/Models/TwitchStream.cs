df17c8ee4558daa830d1326d7de96d207e99489d
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:35:57 +0200

    update file

---
+++ LiveBot.Watcher.Twitch/Models/TwitchStream.cs
@@ -0,0 +1,29 @@
+using LiveBot.Core.Repository.Base.Monitor;
+using LiveBot.Core.Repository.Interfaces.Monitor;
+using LiveBot.Core.Repository.Static;
+using TwitchLibStreams = TwitchLib.Api.Helix.Models.Streams.GetStreams;
+
+namespace LiveBot.Watcher.Twitch.Models
+{
+    /// <summary>
+    /// Represents a Twitch Stream
+    /// </summary>
+    public class TwitchStream : BaseLiveBotStream
+    {
+        public TwitchStream(string baseURL, ServiceEnum serviceType, TwitchLibStreams.Stream stream, ILiveBotUser user, ILiveBotGame game) : base(baseURL, serviceType)
+        {
+            UserId = user.Id;
+            User = user;
+            Id = stream.Id;
+            Title = stream.Title;
+            StartTime = stream.StartedAt;
+            GameId = game.Id;
+            Game = game;
+            ThumbnailURL = stream.ThumbnailUrl;
+            StreamURL = $"{User.ProfileURL}";
+        }
+
+        public TwitchStream()
+        { }
+    }
+}
