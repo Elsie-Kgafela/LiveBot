00227c0b4d605e7330357a9b2e7a08a934dc27b1
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:35:36 +0200

    update file

---
+++ LiveBot.Repository/Migrations/20200608011257_Add ICollection foreign keys for StreamSubscription.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LiveBot.Repository.Migrations
+{
+    public partial class AddICollectionforeignkeysforStreamSubscription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
