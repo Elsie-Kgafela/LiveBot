f6ff085f510f4141f9ccd8418361af3297b7a7cf
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:02 +0200

    update file

---
+++ LiveBot.Core/Repository/Interfaces/IAuthRepository.cs
@@ -0,0 +1,11 @@
+using LiveBot.Core.Repository.Models;
+
+namespace LiveBot.Core.Repository.Interfaces
+{
+    /// <summary>
+    /// Interface for Database interaction with a Monitor Auth
+    /// </summary>
+    public interface IAuthRepository : IRepository<MonitorAuth>
+    {
+    }
+}
