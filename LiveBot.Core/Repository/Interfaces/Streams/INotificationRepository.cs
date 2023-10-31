6b540978ffcd877d6ad9e6829f227e545dc254a9
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:19 +0200

    update file

---
+++ LiveBot.Core/Repository/Interfaces/Streams/INotificationRepository.cs
@@ -0,0 +1,11 @@
+using LiveBot.Core.Repository.Models.Streams;
+
+namespace LiveBot.Core.Repository.Interfaces.Streams
+{
+    /// <summary>
+    /// Interface for Database interaction with a Stream Notification
+    /// </summary>
+    public interface INotificationRepository : IRepository<StreamNotification>
+    {
+    }
+}
