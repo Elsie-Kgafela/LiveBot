37b0989f19cee0f9642aedd034e667d895476230
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:06 +0200

    update file

---
+++ LiveBot.Core/Repository/Static/Queues.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace LiveBot.Core.Repository.Static
+{
+    public class Queues
+    {
+        public static string QueueURL
+        {
+            get => $"rabbitmq://{Environment.GetEnvironmentVariable("RabbitMQ_URL")}";
+        }
+
+        public static readonly string QueueUsername = Environment.GetEnvironmentVariable("RabbitMQ_Username");
+        public static readonly string QueuePassword = Environment.GetEnvironmentVariable("RabbitMQ_Password");
+
+        public static readonly ushort PrefetchCount = 32;
+
+        public static readonly string DiscordAlert = "discord_alert";
+        public static readonly string DiscordAlertChannel = "discord_alertchannel";
+
+        public static readonly string DiscordGuildAvailable = "discord_guildavailable";
+        public static readonly string DiscordGuildUpdate = "discord_guildupdate";
+        public static readonly string DiscordGuildDelete = "discord_guilddelete";
+        public static readonly string DiscordChannelUpdate = "discord_channelupdate";
+        public static readonly string DiscordChannelDelete = "discord_channeldelete";
+        public static readonly string DiscordRoleDelete = "discord_roledelete";
+
+        public static readonly string DiscordMemberLive = "discord_memberlive";
+
+        public static readonly string StreamOnlineQueueName = "streamonline";
+        public static readonly string StreamUpdateQueueName = "streamupdate";
+        public static readonly string StreamOfflineQueueName = "streamoffline";
+    }
+}
