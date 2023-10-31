532f2680c9589865862238e44f120ad4d09445bf
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:13 +0200

    update file

---
+++ LiveBot.Discord.SlashCommands/Contracts/Discord/DiscordMemberLive.cs
@@ -0,0 +1,16 @@
+using LiveBot.Core.Contracts.Discord;
+
+#nullable disable
+
+namespace LiveBot.Discord.SlashCommands.Contracts.Discord
+{
+    public class DiscordMemberLive : IDiscordMemberLive
+    {
+        public ulong DiscordUserId { get; set; }
+        public string Url { get; set; }
+        public string GameName { get; set; }
+        public string GameDetails { get; set; }
+
+        public DateTime LiveTime { get; set; } = DateTime.UtcNow;
+    }
+}
