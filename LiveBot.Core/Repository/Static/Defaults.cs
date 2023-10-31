1c4d649c46a7efb1ff22afe7b5e47b55104f5a81
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:05 +0200

    update file

---
+++ LiveBot.Core/Repository/Static/Defaults.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace LiveBot.Core.Repository.Static
+{
+    public static class Defaults
+    {
+        public static readonly TimeSpan MessageTimeout = TimeSpan.FromMinutes(1);
+        public const string NotificationMessage = "{role} {name} is live and playing {game}! {url}";
+    }
+}
