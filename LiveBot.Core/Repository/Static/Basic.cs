baf72bed5b888228c3f842036aef9e5e4fc5c45e
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:05 +0200

    update file

---
+++ LiveBot.Core/Repository/Static/Basic.cs
@@ -0,0 +1,10 @@
+namespace LiveBot.Core.Repository.Static
+{
+    public static class Basic
+    {
+        public static string SourceLink = "https://github.com/bsquidwrd/LiveBot";
+        public static string WebsiteLink = "https://github.com/bsquidwrd/LiveBot";
+        public static string SupportInvite = "https://discord.gg/zXkb4JP";
+        public static string DonationLink = "https://paypal.me/bsquidwrd";
+    }
+}
