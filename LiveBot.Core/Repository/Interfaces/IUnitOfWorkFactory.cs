ad195d91d55a6719f2d6542f52a22cc4e6cbfdc8
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:04 +0200

    update file

---
+++ LiveBot.Core/Repository/Interfaces/IUnitOfWorkFactory.cs
@@ -0,0 +1,19 @@
+namespace LiveBot.Core.Repository.Interfaces
+{
+    /// <summary>
+    /// Factory wrapper for a Database Factory
+    /// </summary>
+    public interface IUnitOfWorkFactory
+    {
+        /// <summary>
+        /// Used to create an instance of the Database
+        /// </summary>
+        /// <returns></returns>
+        IUnitOfWork Create();
+
+        /// <summary>
+        /// Used to apply migrations to an instance of the Database
+        /// </summary>
+        void Migrate();
+    }
+}
