bbf4a9dae0f4771f58b49fe6635943027fc30809
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:24 +0200

    update file

---
+++ LiveBot.Core/Repository/Models/Streams/StreamUser.cs
@@ -0,0 +1,19 @@
+using LiveBot.Core.Repository.Static;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace LiveBot.Core.Repository.Models.Streams
+{
+    public class StreamUser : BaseModel<StreamUser>
+    {
+        public ServiceEnum ServiceType { get; set; }
+        public string SourceID { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string AvatarURL { get; set; }
+        public string ProfileURL { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<StreamSubscription> StreamSubscriptions { get; set; }
+    }
+}
