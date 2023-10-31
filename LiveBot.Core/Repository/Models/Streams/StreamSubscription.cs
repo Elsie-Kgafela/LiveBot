0ce315d8c7647c8dac4f3f7fef46284166f126d5
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:24 +0200

    update file

---
+++ LiveBot.Core/Repository/Models/Streams/StreamSubscription.cs
@@ -0,0 +1,17 @@
+using LiveBot.Core.Repository.Models.Discord;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace LiveBot.Core.Repository.Models.Streams
+{
+    public class StreamSubscription : BaseModel<StreamSubscription>
+    {
+        public virtual StreamUser User { get; set; }
+        public virtual DiscordGuild DiscordGuild { get; set; }
+        public virtual DiscordChannel DiscordChannel { get; set; }
+        public string Message { get; set; }
+
+        [JsonIgnore]
+        public virtual ICollection<RoleToMention> RolesToMention { get; set; }
+    }
+}
