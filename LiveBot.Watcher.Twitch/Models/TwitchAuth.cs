a6c69e9ef32d745795247a3ff84ef56cad1aa03d
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:35:56 +0200

    update file

---
+++ LiveBot.Watcher.Twitch/Models/TwitchAuth.cs
@@ -0,0 +1,22 @@
+using LiveBot.Core.Repository.Models;
+using LiveBot.Core.Repository.Static;
+using TwitchLib.Api.Auth;
+
+namespace LiveBot.Watcher.Twitch.Models
+{
+    public class TwitchAuth : MonitorAuth
+    {
+        public TwitchAuth(ServiceEnum serviceType, string clientId, RefreshResponse refreshResponse)
+        {
+            ServiceType = serviceType;
+            Expired = false;
+            ClientId = clientId;
+            AccessToken = refreshResponse.AccessToken;
+            RefreshToken = refreshResponse.RefreshToken;
+            ExpiresAt = DateTime.UtcNow.AddSeconds(refreshResponse.ExpiresIn);
+        }
+
+        public TwitchAuth()
+        { }
+    }
+}
