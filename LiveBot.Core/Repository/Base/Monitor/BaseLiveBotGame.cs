18f50807cab74c553609c71597955d19c3848000
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:14 +0200

    update file

---
+++ LiveBot.Core/Repository/Base/Monitor/BaseLiveBotGame.cs
@@ -0,0 +1,29 @@
+using LiveBot.Core.Repository.Interfaces.Monitor;
+using LiveBot.Core.Repository.Static;
+
+namespace LiveBot.Core.Repository.Base.Monitor
+{
+    /// <summary>
+    /// Base implementation of <c>ILiveBotGame</c>
+    /// </summary>
+    public abstract class BaseLiveBotGame : BaseLiveBot, ILiveBotGame
+    {
+        public BaseLiveBotGame(string baseURL, ServiceEnum serviceType) : base(baseURL, serviceType)
+        {
+        }
+
+        public BaseLiveBotGame()
+        { }
+
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ThumbnailURL { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ServiceType}: {Name}";
+        }
+    }
+}
