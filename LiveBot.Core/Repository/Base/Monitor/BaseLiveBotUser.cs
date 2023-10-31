ee312933bcde40cd07acf748a3be72e9a8a76f3d
Elsie-Kgafela <133494419+Elsie-Kgafela@users.noreply.github.com>
2023-10-31 12:36:15 +0200

    update file

---
+++ LiveBot.Core/Repository/Base/Monitor/BaseLiveBotUser.cs
@@ -0,0 +1,29 @@
+using LiveBot.Core.Repository.Interfaces.Monitor;
+using LiveBot.Core.Repository.Static;
+
+namespace LiveBot.Core.Repository.Base.Monitor
+{
+    /// <summary>
+    /// Base implementation of <c>ILiveBotUser</c>
+    /// </summary>
+    public abstract class BaseLiveBotUser : BaseLiveBot, ILiveBotUser
+    {
+        public BaseLiveBotUser(string baseURL, ServiceEnum serviceType) : base(baseURL, serviceType)
+        {
+        }
+
+        public BaseLiveBotUser()
+        { }
+
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string AvatarURL { get; set; }
+        public string ProfileURL { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ServiceType}: {Username}";
+        }
+    }
+}
